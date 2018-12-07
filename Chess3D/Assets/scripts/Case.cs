063322bd0cf6764d4b0fa0a14dc3b844f59dbe94
Dagaubs <alan181096@gmail.com>
2018-12-07 22:37:12 +0100

    Create Unity project and A Game Manager that create the gameplate

---
+++ Chess3D/Assets/scripts/Case.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Case : MonoBehaviour {
+
+	public enum case_type{
+		LIGHT,
+		DARK
+	}
+
+	[SerializeField]
+	private case_type type;
+
+	public case_type getType(){return type;}
+
+	private int indexInPlate;
+
+	public void init(int nb){
+		transform.position = new Vector3(-4 + (nb%8), 0,-4 + (nb/8));
+	}
+}
---
+++ Chess3D/Assets/scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject plate;
+
+	[SerializeField]
+	private GameObject light_case, dark_case;
+
+	private Case[] cases;
+
+	void Start(){
+		cases = new Case[64];
+		initPlate();
+	}
+
+	void initPlate(){
+		bool isLight = true;
+		for(int i = 0; i < 64; i++){
+			GameObject caseGo = isLight ? light_case : dark_case;
+			Case createCase = Instantiate(caseGo, plate.transform, false).GetComponent<Case>();
+			createCase.init(i);
+			cases[i] = createCase;
+			if((i+1) % 8 != 0)
+				isLight = !isLight;
+		}
+	}
+}
