46fe6522a566a95d06c93462d890c1588e12fd2a
prune <prunefd@gmail.com>
2019-02-04 15:09:34 +0100

    tada

---
+++ Chess3D/Assets/scripts/CameraFPS.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFPS : MonoBehaviour {
+
+	public float speed;
+	public float sensitivity;
+
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.Z)){
+			transform.position += Vector3.up *speed;
+			Debug.LogWarning("up");
+		}
+		if(Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S)){
+			transform.position -= Vector3.up *speed;
+			Debug.LogWarning("down");
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.Q)){
+			transform.position -= Vector3.right *speed;
+			Debug.LogWarning("left");
+		}
+		if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D)){
+			transform.position += Vector3.right *speed;
+			Debug.LogWarning("right");
+		}
+
+	 	Vector3 vp = cam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
+ 		vp.x -= 0.5f; vp.y -= 0.5f;
+ 		vp.x *= sensitivity; vp.y *= sensitivity;
+ 		vp.x += 0.5f; vp.y += 0.5f;
+ 		Vector3 sp = cam.ViewportToScreenPoint(vp);
+ 
+ 		Vector3 v = cam.ScreenToWorldPoint(sp);
+ 		transform.LookAt(v, Vector3.up);
+	}
+}
